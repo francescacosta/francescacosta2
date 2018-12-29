07c3698ff6c040115ead8dc221d8cca8903cb077
Francesca <francesca@thrivewb.com.au>
2018-12-29 16:15:46 +1000

    comment out instagram, add name and youtube

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,15 @@ export default ({ globalSettings, socialSettings, navLinks }) => (
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
     </h2>
     <br />
-    <InstagramFeed count="8" />
+    // <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://www.youtube.com/channel/UCAhrQnqRvvTyPhHWeuB3wnw?">
+            Francesca Costa
+          </a>
+          .
         </span>
       </div>
     </footer>
